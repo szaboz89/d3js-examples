266401edcbdb66d0a9b14e91ef65d43adcb3ebe0
Zoltán Szabó <zoltan.szabo@atolldev.hu>
2018-04-20 08:31:41 +0200

    Visualize buildings height Load data from json Convert height to number Add rectangles with different heights base on loaded data

---
+++ 03-buildings/js/main.js
@@ -0,0 +1,26 @@
+var svg = d3.select("#chart-area")
+    .append("svg")
+    .attr("width", "400")
+    .attr("height", "400");
+
+d3.json("data/buildings.json").then(function (data) {
+    data.forEach(function (d) {
+        d.height = +d.height;
+    });
+
+    var rects = svg.selectAll("rect").data(data);
+
+    rects.enter().append("rect")
+        .attr("x", function (d, i) {
+            return (i * 60);
+        })
+        .attr("y", 50)
+        .attr("width", 40)
+        .attr("height", function (d) {
+            return d.height;
+        })
+        .attr("fill", "grey");
+
+}).catch(function (error) {
+    console.log(error);
+});
