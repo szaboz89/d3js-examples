3d24efb40a749e20a5c3167b37e4208b189406b5
Zoltán Szabó <zoltan.szabo@atolldev.hu>
2018-04-23 08:23:27 +0200

    Axis and labels Add labels and axis Reverse y scale and rectangles

--- 03-buildings/js/main.js
+++ 03-buildings/js/main.js
@@ -1,4 +1,4 @@
-let margin = {left: 100, right: 10, top: 10, bottom: 100};
+let margin = {left: 100, right: 10, top: 10, bottom: 150};
 
 let width = 600 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
@@ -9,6 +9,25 @@ let svg = d3.select("#chart-area")
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+
+// X Label
+svg.append("text")
+    .attr("class", "x axis-label")
+    .attr("x", width / 2)
+    .attr("y", height + 140)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .text("The word's tallest buildings");
+
+// Y Label
+svg.append("text")
+    .attr("class", "y axis-label")
+    .attr("x", -(height / 2))
+    .attr("y", -60)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(-90)")
+    .text("Height (m)");
 
 d3.json("data/buildings.json").then((data) => {
     data.forEach(d => {
@@ -21,7 +40,7 @@ d3.json("data/buildings.json").then((data) => {
         .domain([0, d3.max(data, (d) => {
             return d.height;
         })])
-        .range([0, height]);
+        .range([height, 0]);
 
     let x = d3.scaleBand()
         .domain(data.map((d) => {
@@ -35,12 +54,34 @@ d3.json("data/buildings.json").then((data) => {
         .attr("x", (d) => {
             return x(d.name);
         })
-        .attr("y", 50)
-        .attr("width", x.bandwidth())
-        .attr("height", (d) => {
+        .attr("y", (d) => {
             return y(d.height);
         })
+        .attr("width", x.bandwidth())
+        .attr("height", (d) => {
+            return height - y(d.height);
+        })
         .attr("fill", "grey");
+
+    let xAxisCall = d3.axisBottom(x);
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0, " + height + ")")
+        .call(xAxisCall)
+        .selectAll("text")
+        .attr("y", "10")
+        .attr("x", "-5")
+        .attr("text-anchor", "end")
+        .attr("transform", "rotate(-40)");
+
+    let yAxisCall = d3.axisLeft(y)
+        .ticks(3)
+        .tickFormat((d) => {
+            return d + " m";
+        });
+    svg.append("g")
+        .attr("class", "y-axis")
+        .call(yAxisCall);
 
 }).catch((error) => {
     console.log(error);
