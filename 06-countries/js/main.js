8096734b98dbf6e139e07ed7b3d038739d2c4250
Zoltán Szabó <zoltan.szabo@atolldev.hu>
2018-05-22 12:48:47 +0200

    jQuery UI slider for year Add jQuery UI library Add slider for year

--- 06-countries/js/main.js
+++ 06-countries/js/main.js
@@ -140,6 +140,16 @@ $("#continent-select").on("change", function () {
     update(formattedData[time]);
 });
 
+$("#date-slider").slider({
+    max: 2014,
+    min: 1800,
+    step: 1,
+    slide: function (event, ui) {
+        time = ui.value - 1800;
+        update(formattedData[time]);
+    }
+});
+
 function step() {
     // At the end of our data, loop back
     time = (time < 214) ? time + 1 : 0;
@@ -195,4 +205,7 @@ function update(data) {
 
     // Update the time label
     timeLabel.text(+(time + 1800))
+
+    $("#year")[0].innerHTML = +(time + 1800)
+    $("#date-slider").slider("value", +(time + 1800))
 }
